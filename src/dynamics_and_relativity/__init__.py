ed38d93d81b74cb47abb44889588c499622c53eb
Jonathan <morimemento406@gmail.com>
2026-09-16 14:46:45 +0200

    Changes linear regression and plotting

--- src/dynamics_and_relativity/__init__.py
+++ src/dynamics_and_relativity/__init__.py
@@ -1,11 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
-import polars_ds as pds
+import statsmodels.api as sm
 
 
 def main() -> None:
     # get data
-    data_file = input("Data:")
+    data_file ="/Users/jmeij/Downloads/test.txt"
     distance = float(input("Totale afstand(cm):"))
 
 
@@ -14,22 +15,40 @@ def main() -> None:
     pixel_distance = ((data.get_column("x").item(0) - data.get_column("x").item(-1))**2 +(data.get_column("y").item(0) - data.get_column("y").item(-1))**2)**0.5
 
     #insert columns
-    data.insert_column(1, (pl.col("frame")/frame_rate).alias("time"))
+    data.insert_column(1, ((pl.col("frame")-1)/frame_rate).alias("time"))
     data.insert_column(data.shape[1],(pl.col("x").diff()).alias("x_change"))
     data.insert_column(data.shape[1],(pl.col("y").diff()).alias("y_change"))
     data.insert_column(data.shape[1],(np.sqrt(pl.col("x_change")**2 +pl.col("y_change")**2)).alias("velocity(px/s)"))
     data.insert_column(data.shape[1],(pl.col("velocity(px/s)")/pixel_distance*(distance/100)).alias("velocity(m/s)"))
 
+    # create linear fit with velocity as dependency of time.
+    data = data.drop_nulls()
+    time = data["time"].to_numpy()
+    vel  = data["velocity(m/s)"].to_numpy()
+    Time = sm.add_constant(time)
+    model = sm.OLS(vel, Time).fit()
 
-    coeffs: list = data.select(
-        pds.lin_reg(pl.col("time"), target = pl.col("velocity(m/s)"),add_bias = True)
-    ).item(0,0).to_list()
-    result_frame = pl.from_dict({"coefficient" : coeffs[0], "constant":coeffs[1]})
+
+    # save model summary to file.
+    summary = model.summary(
+        xname=["Intercept (v0)", "Acceleration (m^2/s)"],
+        yname="Velocity (m/s)"
+    )
+    with open("reg_sum.txt", "w") as f:
+        f.write(summary.as_txt())
+
 
     ## create plot
-    plot = data.plot.scatter(x="time",y="velocity(px/s)")
-    plot.save("plot.svg")
+    fig, ax= plt.subplots()
+    ax.scatter(
+        x=data["time"],
+        y=data["velocity(m/s)"],
+    )
+    ax.set(xlabel = "T (s)", ylabel="V (m/s)")
+    ax.errorbar(data["time"],data["velocity(m/s)"],yerr=0.1, capsize=5, fmt="ok", label = "data")
+    ax.grid()
+    ax.set_title("Ball speed")
+    fig.savefig("./plot.svg")
 
     ##write to output
     data.write_csv("./output.csv", separator = ",")
-    result_frame.write_csv("./lin_reg.csv",separator=",")
