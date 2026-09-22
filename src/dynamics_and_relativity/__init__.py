a5d926367337713628832010c5a42536ec312ac5
Jonathan <morimemento406@gmail.com>
2026-09-22 20:56:54 +0200

    added error propagation

--- src/dynamics_and_relativity/__init__.py
+++ src/dynamics_and_relativity/__init__.py
@@ -5,12 +5,14 @@ import numpy as np
 import polars as pl
 import statsmodels.api as sm
 
+from dynamics_and_relativity.err_prop import Formula
+
 
 def main() -> None:
     #loop through all 10 measurements
-    for i in range(1,11):
+    for i in range(2,3):
         # get data
-        data_file =f"/path/to/Results meting {i}.csv"
+        data_file =f"/Users/jmeij/Downloads/OneDrive_1_9-16-2026/Results meting {i}.csv"
         output_folder = f"./results/meting{i}/"
         #distance = float(input("Totale afstand(cm):"))
         os.makedirs(output_folder, exist_ok=True)
@@ -18,16 +20,23 @@ def main() -> None:
         frame_rate = 400
         raw_data= pl.read_csv(data_file, has_header=True, separator=",", row_index_name= "frame")
         data= raw_data.select(["frame","X","Y"])
+        formula = Formula("sqrt(x_change**2 + y_change**2) / frametime",vars = ["x_change","y_change"], consts={"frametime":1/frame_rate})
 
         #data = pl.read_csv(data_file, has_header= False,separator="\t", schema= {"x": pl.Float64, "y":pl.Float64}, row_index_name="frame")
         pixel_distance = ((data.get_column("X").item(0) - data.get_column("X").item(-1))**2 +(data.get_column("Y").item(0) - data.get_column("Y").item(-1))**2)**0.5
 
         #insert columns
-        data.insert_column(1, ((pl.col("frame")-1)/frame_rate).alias("time"))
+        data.insert_column(1, ((pl.col("frame"))/frame_rate).alias("time"))
         data.insert_column(data.shape[1],(pl.col("X").diff()).alias("x_change"))
         data.insert_column(data.shape[1],(pl.col("Y").diff()).alias("y_change"))
         data.insert_column(data.shape[1],(np.sqrt(pl.col("x_change")**2 +pl.col("y_change")**2)*frame_rate).alias("velocity(cm/s)"))
         data.insert_column(data.shape[1],(pl.col("velocity(cm/s)")/100).alias("velocity(m/s)"))
+        data.insert_column(data.shape[1], pl.lit(0.1).alias("delta_x_change"))
+        data.insert_column(data.shape[1], pl.lit(0.1).alias("delta_y_change"))
+        err_formula = formula.get_error_expr()
+        print(data)
+        print(err_formula)
+        print(err_formula.evaluate_polars(data.drop_nans().drop_nulls()).alias("velocity"))
 
         # create linear fit with velocity as dependency of time.
         data = data.drop_nulls()
@@ -43,6 +52,20 @@ def main() -> None:
             yname="X"
         )
         with open(output_folder+"reg_sum.txt", "w") as f:
+            f.write(summary.as_text())
+        data = data.drop_nulls()
+        time = data["time"].to_numpy()
+        vel  = data["velocity(m/s)"].to_numpy()
+        Time = sm.add_constant(time)
+        model = sm.OLS(vel, Time).fit()
+
+
+        # save model summary to file.
+        summary = model.summary(
+            xname=["Intercept (v0)", "velocity (m*s^-2)"],
+            yname="V"
+        )
+        with open(output_folder+"reg_sum_v.txt", "w") as f:
             f.write(summary.as_text())
 
 
@@ -64,3 +87,6 @@ def main() -> None:
 
         ##write to output
         data.write_csv(output_folder+"/output.csv", separator = ",")
+
+if __name__ == "__main__":
+    main()
---
+++ src/dynamics_and_relativity/err_prop.py
@@ -0,0 +1,109 @@
+import numpy as np
+import polars as pl
+import sympy as sp
+from sympy import Basic
+
+
+class Formula:
+    def __init__(self,formula: str|sp.Expr, vars: list[str], consts: dict[str, float] | None) -> None:
+        self.symbols = {
+            name : sp.Symbol(name)
+            for name in list((consts or {}).keys()) + vars
+        }
+        self.vars = vars
+        self.consts = consts or {}
+        if isinstance(formula, str):
+            self.formula = sp.parse_expr(formula, local_dict=self.symbols)
+            self._formula_str = formula
+        elif isinstance(formula, sp.Expr):
+            self.formula = formula
+            self._formula_str = str(formula)
+        else:
+            raise TypeError(f"Unsupported formula type: {type(formula)}")
+    def __str__(self):
+        return self._formula_str + "\n vars:" + self.vars.__str__() + "\n consts:" + self.consts.__str__()
+
+
+    def get_error_expr(self, vars:list[str]|None = None) -> Formula:
+        err_sqrd = 0
+        delta_vars: dict[str,sp.Symbol] = {}
+        variables = vars or self.vars
+        for var in variables:
+            symbol = self.symbols[var]
+            delta = sp.Symbol(f"delta_{var}")
+            derivative = sp.diff(self.formula, symbol)
+            err_sqrd += (derivative * delta) ** 2
+            delta_vars[f"delta_{var}"] =delta
+
+        err: sp.Expr = sp.sqrt(err_sqrd)
+        err = err.simplify()
+        return Formula(err,list(delta_vars.keys()) + self.vars,consts = self.consts)
+
+
+
+    def update_formula(self, new_str:str):
+        self.formula = sp.parse_expr(new_str, local_dict=  self.symbols)
+        self._formula_str = new_str
+    def update_vars(self, vars:list[str]):
+        self.vars = vars
+        self.symbols = {
+            name : sp.Symbol(name)
+            for name in list((self.consts or {}).keys()) + vars
+        }
+        self.formula :sp.Expr= sp.parse_expr(self._formula_str, local_dict = self.symbols)
+
+    def evaluate(self, var_values: dict[str,float]):
+        varsdict:dict[Basic, Basic | float] = {
+            self.symbols[name] : value
+            for name, value in var_values.items() if name in self.symbols
+        }
+        constsdict:dict[Basic, Basic | float] = {
+            self.symbols[name] : value
+            for name, value in self.consts.items() if name in self.symbols
+        }
+        symbolsdict: dict[Basic, Basic | float] = varsdict | constsdict
+        return self.formula.evalf(subs= symbolsdict)
+
+    def evaluate_polars(self, df: pl.DataFrame, result_name="result"):
+        expr = self.formula.subs({
+            self.symbols[name]: value
+            for name, value in self.consts.items()
+        })
+
+        args = [self.symbols[var] for var in self.vars]
+
+        func = sp.lambdify(
+            args,
+            expr,
+            modules=["numpy"]
+        )
+
+        arrays = [
+            df[var].cast(pl.Float64).to_numpy()
+            for var in self.vars
+        ]
+
+        values = func(*arrays)
+
+        return pl.Series(result_name, values)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    formula1 = "( x**2 + sin(y)**2 )**0.5"
+    form = Formula(formula1, vars=["x","y"], consts={})
+    res = form.evaluate(var_values={"x":10.0,"y":10.0})
+    err_func = form.get_error_expr()
+    err_func_eval = err_func.evaluate(var_values={
+        "x":10,
+        "delta_x":2.0,
+        "y":10.0,
+        "delta_y":0.5,
+    })
+    print(err_func_eval)
